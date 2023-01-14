8365f6208aeb33aab0397cd74ac6da4139adeb2b
denisQa322 <denis.isakov.99@gmail.com>
2023-01-14 13:40:49 +0600

    registrtation added

---
+++ index.js
@@ -0,0 +1,24 @@
+const registrationButton = document.querySelector('.registration');
+const inputName = document.querySelector('.name');
+const inputCity = document.querySelector('.city');
+const inputPass = document.querySelector('.password');
+const registrationWrapper = document.getElementById('registration-wrapper');
+const body = document.querySelector('body');
+
+const checkForReg = sessionStorage.getItem('user.data');
+
+if (checkForReg){
+    alert('You have already registrated');
+    alert('Logging in');
+    registrationWrapper.style.display = 'none';
+}
+
+registrationButton.addEventListener('click', () => {
+    const data ={
+        name: inputName.value,
+        city: inputCity.value,
+        password: inputPass.value
+    }
+
+    sessionStorage.setItem('user.data', JSON.stringify(data));
+});
